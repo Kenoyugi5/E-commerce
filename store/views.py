3cf19b9953bd807537c1debaf625226f8142f5d9
Kenoyugi5 <koyugi02@gmail.com>
2024-06-10 14:47:19 +0300

    Base html file

--- store/views.py
+++ store/views.py
@@ -4,3 +4,6 @@ from .models import Product
 def home(request):
 	products = Product.objects.all()
 	return render(request, 'store/home.html', {'products': products})
+
+def about(request):
+	return render(request, 'store/about.html', {})
